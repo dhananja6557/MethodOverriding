44208d192f985d497df8bfa58d0a5e14e54c181b
dhananja6557 <info.bizlanka@gmail.com>
2023-03-31 11:46:53 +0530

    MethodOverriding

--- src/Monkey.java
+++ src/Monkey.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Monkey {
+public class Monkey {
+    Monkey(){}
+    void climb() {
+        System.out.println("Using body!");
+    }
+    void eat() {
+        System.out.println("Eating!");
+    }
+}
+class Man extends Monkey {
+    Man(){}
+    void climb() {
+        System.out.println("Using ladder!");
+        super.climb();
+        super.eat();
+    }
+}
+class TestOutput {
+    public static void main(String[] args) {
+        Man m = new Man();
+        Monkey mky = new Monkey();
+        m.climb();
+        mky.climb();
+    }
 }
